adc20e2fc4aab20ab9af625b2ba86fba799e47ee
VghostS <92262059+VghostS@users.noreply.github.com>
2025-03-20 11:37:42 +0530

    Add files via upload

---
+++ telegram_bot.py
@@ -0,0 +1,33 @@
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+import os
+
+# Define the start function
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('HI')
+
+def EXE(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('EXE')
+
+def main() -> None:
+    # Get the bot token from the environment variable
+    token = os.getenv("7777540525:AAFbd5VM_4qe6k94nTrLii4-FixCKKC4qFU")
+    if not token:
+        raise ValueError("No token provided")
+
+    updater = Updater(token)
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # Register the /start command handler
+    dispatcher.add_handler(CommandHandler("start", start))
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT, SIGTERM or SIGABRT
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
